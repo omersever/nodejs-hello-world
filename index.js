7e60cf90b654fad3980243e54f936ca926f7dbcf
omersever@gmail.com <omers@tr.ibm.com>
2020-07-22 11:48:46 +0300

    commit

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ var server = http.createServer(function(request, response) {
     response.writeHead(200, {"Content-Type": "text/plain"});
     response.end("Hello World!");
 
+    console.log("Ozan Bey")
+
 });
 
 var port = 8080;
